6da37a9c7381d0ab29a1e4cf91707d4b4bb18852
Shinya Hara <s.hara@sonicmoov.com>
2021-09-18 01:28:29 +0900

    chore: initial commit

---
+++ android/app/src/main/kotlin/com/example/hara/webview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hara.webview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        body: WebViewScreen(),
+      ),
+    );
+  }
+}
+
+class WebViewScreen extends StatefulWidget {
+  @override
+  WebViewScreenState createState() => WebViewScreenState();
+}
+
+class WebViewScreenState extends State<WebViewScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: WebView(
+        initialUrl: 'https://www.youtube.com/',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
